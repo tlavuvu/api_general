7b59a74dcc21aaef06f6c79ea436f0c012152f16
Vincent Seitlhamo <vseitlhamo@analytikus.co.za>
2024-10-31 14:34:50 +0200

    commit

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -34,33 +34,29 @@ export const signup = async (
   res.json(user)
 }
 
-export const login = async (
-  req: Request,
-  res: Response) => {
-  const { email, password } = req.body
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body
 
-  let user = await prismaClient.user.findFirst({ where: { email } })
-  if (!user) {
-    throw new NotFoundException('User not found', errorCode.USER_NOT_FOUND)
-  }
-  if (!compareSync(password, user.password)) {
-    throw new BadRequestsException(
-      'Incorrect Username/Password',
-      errorCode.INCORRECT_PASSWORD
+    let user = await prismaClient.user.findFirst({ where: { email } })
+    if (!user) {
+      throw new NotFoundException('User not found', errorCode.USER_NOT_FOUND)
+    }
+    if (!compareSync(password, user.password)) {
+      throw new BadRequestsException(
+        'Incorrect Username/Password',
+        errorCode.INCORRECT_PASSWORD
+      )
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      JWT_SECRET
     )
-  }
-  const token = jwt.sign(
-    {
-      userId: user.id,
-    },
-    JWT_SECRET
-  )
 
   res.json({ user, token })
 }
 
-// export const me = async (req: Request, res: Response) => {
-//   res.json(req.user)
-// }
-
-// /me -> return the logged in user
+export const me = async (req: Request, res: Response) => {
+  res.json(req.user)
+}
